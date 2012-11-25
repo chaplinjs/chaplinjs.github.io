a98e9182608e9f2c9781f65ea9bc364077a824b6
Paul Miller <paul@paulmillr.com>
2012-11-25 02:51:09 +0200

    Initial commit.

---
+++ javascripts/main.js
@@ -0,0 +1,123 @@
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller
+// fixes from Paul Irish and Tino Zijdel
+
+(function() {
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+  }
+ 
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function(callback, element) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+        timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  }
+ 
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function(id) {
+      clearTimeout(id);
+    };
+  }
+}());
+
+var sectionHeight = function() {
+  var total    = $(window).height();
+  var $section = $('section').css('height','auto');
+
+  if ($section.outerHeight(true) < total) {
+    var margin = $section.outerHeight(true) - $section.height();
+    $section.height(total - margin - 20);
+  } else {
+    $section.css('height','auto');
+  }
+};
+
+$(window).resize(sectionHeight);
+
+var listenForScroll = function() {
+  var lastScrollY = 0;
+  var ticking = false;
+  var wrapper = $('.wrapper');
+  var nav = wrapper.find('> nav');
+
+  var update = function() {
+    if (lastScrollY > 230) {
+      nav.css({position: 'fixed'});
+    } else {
+      nav.css({position: 'absolute'});
+    }
+    ticking = false;
+  };
+
+  var requestTick = function() {
+    if (!ticking) {
+      requestAnimationFrame(update);
+      ticking = true;
+    }
+  };
+
+  var onScroll = function() {
+    lastScrollY = window.scrollY;
+    requestTick();
+  };
+
+  $(window).on('scroll', onScroll);
+};
+
+$(document).ready(function(){
+  $("section h1, section h2").each(function(){
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+    $("nav ul li:first-child a").parent().addClass("active");
+  });
+  
+  $("nav ul li").on("click", "a", function(event) {
+    var position = $($(this).attr("href")).offset().top - 20;
+    var scroll = function(element) {
+      $(element).animate({scrollTop: position}, 400);
+    };
+    scroll("html");
+    scroll("body");
+    $("nav ul li a").parent().removeClass("active");
+    $(this).parent().addClass("active");
+    event.preventDefault();    
+  });
+  
+  sectionHeight();
+  
+  $('img').load(sectionHeight);
+
+  listenForScroll();
+});
+
+fixScale = function(doc) {
+
+  var addEvent = 'addEventListener',
+      type = 'gesturestart',
+      qsa = 'querySelectorAll',
+      scales = [1, 1],
+      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+
+  function fix() {
+    meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+    doc.removeEventListener(type, fix, true);
+  }
+
+  if ((meta = meta[meta.length - 1]) && addEvent in doc) {
+    fix();
+    scales = [.25, 1.6];
+    doc[addEvent](type, fix, true);
+  }
+
+};
